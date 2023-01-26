557fc9d1d3e4322fbef6048a772221151545468a
Najma <najma.a16@gmail.com>
2023-01-26 15:17:12 -0600

    components fixed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,6 @@
 import React, { useState } from "react";
-import {BrowserRouter as Switch, Route} from "react-router-dom";
-// import { Switch, Routes } from "react-router-dom";
 import Home from "./Home";
-import Login from "./Login";
-import Signup from "./Signup";
-import NavBar from "./NavBar";
-import DestinationsPage from "./DestinationsPage";
-import Profile from "./Profile";
+
 
 
 
@@ -16,47 +10,7 @@ function App() {
   return (
       
     <div>
-      <NavBar onChangePage={setPage} />
-      <Switch>
-        <Route exact path="/"
-        element={
-          <div>
-            <Home/>
-            </div>
-        }>
-        
-        </Route>
-        <Route path="/signup"element={
-          <div>
-            <Signup/>
-            </div>
-        }>
-        </Route>
-        <Route path="/login"element={
-          <div>
-            <Login/>
-            </div>
-        }>
-        </Route>
-        <Route path="/destinations"element={
-          <div>
-            <DestinationsPage/>
-            </div>
-        }>
-        </Route>
-        <Route path="/profile"element={
-          <div>
-            <Profile/>
-            </div>
-        }>
-        </Route>
-        <Route path="*"element={
-          <h1>404 not found</h1>
-        }>
-      
-        </Route>
-      </Switch>
-
+     <Home/>
     </div>
     
   );
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,63 @@
-import React from 'react'
-import Signup from './Signup'
-import Login from './Login'
-
+import React, { useState } from "react";
+import {BrowserRouter as Switch, Route} from "react-router-dom";
+// import { Switch, Routes } from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
+import NavBar from "./NavBar";
+import DestinationsPage from "./DestinationsPage";
+import Profile from "./Profile";
 function Home(){
 
 
-return (
-    <div>
-        <Signup></Signup>   
-        <Login></Login>
-    </div>
-)
+    const [page, setPage] = useState("/");
+
+    return (
+        
+      <div>
+        <NavBar onChangePage={setPage} />
+        <Switch>
+          <Route exact path="/"
+          element={
+            <div>
+              <Home/>
+              </div>
+          }>
+          
+          </Route>
+          <Route path="/signup"element={
+            <div>
+              <Signup/>
+              </div>
+          }>
+          </Route>
+          <Route path="/login"element={
+            <div>
+              <Login/>
+              </div>
+          }>
+          </Route>
+          <Route path="/destinations"element={
+            <div>
+              <DestinationsPage/>
+              </div>
+          }>
+          </Route>
+          <Route path="/profile"element={
+            <div>
+              <Profile/>
+              </div>
+          }>
+          </Route>
+          <Route path="*"element={
+            <h1>404 not found</h1>
+          }>
+        
+          </Route>
+        </Switch>
+  
+      </div>
+      
+    );
 }
 
 export default Home
