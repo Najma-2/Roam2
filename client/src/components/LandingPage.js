d02c47e66b02b7faa8cbc3df3d9dd83d86a02feb
Ana Rosario <arosario.mrd@gmail.com>
2023-01-26 19:53:32 -0500

    css

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,19 +1,20 @@
 import React from 'react'
-
+import Home from './Home'
 
 function LandingPage(){
-
-
-
-
 
 
     return(
         <div>
             <h1>Roam</h1>
-            <div> Welcome Message</div>
             <div>Login or Signup</div>
+            <h1> Top Destinations to See in 2023</h1>
+            <h4> Email:</h4>
+            <h5> roam@traveldestinations.co</h5>
+            <h4> Web</h4>
+            <h5> roam.com</h5>
         </div>
+        
     )
 }
 
