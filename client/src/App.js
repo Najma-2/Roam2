f5dd7cb78553bb6f8913e6d366b8170fd7e3e3c8
Najma <najma.a16@gmail.com>
2023-01-31 11:10:54 -0600

    css

--- client/src/App.js
+++ client/src/App.js
@@ -54,7 +54,7 @@ function handleNewDestination(newDest){
       <Routes>
         <Route exact path='/' element={<div className="landing-page"><AuthModal /></div>} />
         <Route exact path="/destinations" element={<DestinationsContainer destinations={destinations}/>} />
-        <Route exact path="/create" element={<AddDest handleNewDestination={handleNewDestination} user={user} destinations={destinations} newDest={newDest}/>} />
+        <Route exact path="/destinations" element={<AddDest handleNewDestination={handleNewDestination} user={user} destinations={destinations} newDest={newDest}/>} />
         <Route exact path="/profile" element={<Profile user={user} />} />
       </Routes>
     </div>
